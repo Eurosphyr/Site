abafe469374f8ec9e6f30d1df8423accd9e47f58
Eurosphyr <miguel.mmgg2016@gmail.com>
2023-11-17 10:34:43 -0300

    Alterado rodapé

--- html/ec-sobre.php
+++ html/ec-sobre.php
@@ -13,33 +13,100 @@
     <?php include '../php/funcoes.php';
         session_start();
         exibirConteudoComBaseNoPapel();?>
-      <div class="sobre">
-        <div class="apresentacao">Sobre Nós</div>
-        <div class="linha"></div>
-      </div>
-      <div class="part-img1">
-        <div class="imagem1"></div>
-      </div>
-      <div class="escrita1">
-        <div class="escritaL">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          laoreet lobortis pellentesque. Vivamus ut dolor vel tortor tempor
-          aliquet pulvinar sed sem.
-        </div>
-      </div>
-      <div class="part-img2">
-        <div class="imagem2"></div>
-      </div>
-      <div class="escrita2">
-        <div class="escritaD">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          laoreet lobortis pellentesque. Vivamus ut dolor vel tortor tempor
-          aliquet pulvinar sed sem.
-        </div>
-      </div>
-      <div class="part-banner">
-        <div class="banner"></div>
+      <<div class="sobre">
+      <div class="apresentacao">Sobre Nós</div>
+      <div class="linha"></div>
+    </div>
+    <div class="part-img1">
+      <div class="imagem1"></div>
+    </div>
+    <div class="escrita1">
+      <div class="escritaL">
+        A marca MIPRON foi criada a partir
+        das iniciais de seus fundadores MI
+        para Mizael e
+        Miguel, P para Pedro, R para
+        Richard e N para Nicole, a letra O
+        foi inserida para dar
+        a marca o tom e sonoridade mais
+        tecnológico, deste modo a marca
+        ganha um tom
+        autêntico com uma sonoridade
+        mais tecnológica.
       </div>
     </div>
-  </body>
+    <div class="part-img2">
+      <div class="imagem2"></div>
+    </div>
+    <div class="escrita2">
+      <div class="escritaD">
+        A logo da MIPRON baseia se na
+        tendência mais que atual do
+        minimalismo objetivo,
+        que atualmente é a técnica mais
+        dominante no meio da web design
+        que trata se no
+        desenvolvimento da logo a partir
+        das letras da marca,
+        transformando as letras da
+        marca numa logomarca única e
+        que demarque nosso símbolo para
+        todos que o
+        verem, sem nos confundirem com
+        outras empresas. Para manter o
+        objetivo
+        minimalista aplicamos o positivo e
+        negativo com o preto em branco
+        que se aplicou
+        facilmente sob nossa palheta de
+        cores selecionada.
+      </div>
+    </div>
+    <div class="part-banner">
+      <div class="banner"></div>
+    </div>
+    <div class="grupo">
+      <p class="nos">Grupo de Desenvolvedores</p>
+      <div class="p1">
+        <img class="imagemf" src="p1.jpeg">
+        <p>Miguel Angelo de Lima Godoi - Gerente Financeiro / Email: miguel.godoi@unesp.br</p>
+      </div>
+      <div class="p2">
+        <img class="imagemf" src="p2.jpeg">
+        <p>Mizael Martins Barreto - Gerente de Marketing / Email: mizael.martins@unesp.br</p>
+      </div>
+      <div class="p3">
+        <img class="imagemf" src="p3.jpeg">
+        <p>Nicole dos Santos Quadros - Gerente de Qualidade / Email: n.quadros@unesp.br</p>
+      </div>
+      <div class="p4">
+        <img class="imagemf" src="p4.jpeg">
+        <p>Pedro Augusto de Oliveira Galdino Ribeiro - Líder / Email: paog.ribeiro@unesp.br</p>
+      </div>
+      <div class="p5">
+        <img class="imagemf" src="p5.jpeg">
+        <p>Richard Walace de Oliveira Camargo - Líder Técnico e Gerente de Produção / Email: rwo.camargo@unesp.br</p>
+      </div>
+    </div>
+    <div class="professores">
+      <p class="nos">Orientadores</p>
+      <div class="p1">
+        <img class="imagemf" src="pf1.jpeg">
+        <p>Debora Barbosa Aires - Aplicativos I / Email: debora.aires@unesp.br</p>
+      </div>
+      <div class="p2">
+        <img class="imagemf" src="pf2.jpeg">
+        <p>José Vieira Junior - Banco de Dados / Email: vieira.junior@unesp.br</p>
+      </div>
+      <div class="p3">
+        <img class="imagemf" src="pf3.jpeg">
+        <p>Jovita Mercedes Hojas Baenas - Gestão de Negócios / Email: jovita.baenas@unesp.br</p>
+      </div>
+      <div class="p4">
+        <img class="imagemf" src="pf4.jpg">
+        <p>Marcelo Cabello Peres - PHP / Email: marcelo.peres@unesp.br</p>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
